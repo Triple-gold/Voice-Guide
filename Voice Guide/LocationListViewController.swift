862e8b72408fd43d7ad3801e97fa08e16b90a605
Yu Fu Yam <yu121249377@gmail.com>
2022-01-14 23:48:53 +0800

    version

--- Voice Guide/LocationListViewController.swift
+++ Voice Guide/LocationListViewController.swift
@@ -91,6 +91,7 @@ class LocationListViewController: UIViewController, UITableViewDataSource, UITab
             break
         }
     }
+    //prepare the location number to next page
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goLocationDetail"{
             let controller = segue.destination as! LocationDetailViewController
